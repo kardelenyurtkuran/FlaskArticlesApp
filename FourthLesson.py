217fe6e47b48e2a0940c6359648937b0db2e285a
krdlnyrtkrn <kardelenyurtkuran@gmail.com>
2023-11-22 16:47:06 +0300

    Flash Message

--- FourthLesson.py
+++ FourthLesson.py
@@ -23,6 +23,7 @@ connection = pymysql.connect(
     cursorclass=pymysql.cursors.DictCursor
 )
 
+
 # Kullanıcı Kayıt Formu
 class RegisterForm(FlaskForm):
     name = StringField("İsim Soyisim", validators=[Length(min=4, max=25)])
@@ -44,11 +45,26 @@ def about():
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
-    form = RegisterForm(request.form)
-    if request.method == "POST" and form.validate(): #form.validate istenen tüm şartları sağlaması halinde True döner. Formda doldurulan alanlarda bir problem olup olmadığı kontrol edilir
-        return redirect(url_for("index")) #fonksiyon ismine göre belli bir sayfaya göndermek için
-    else:
-        return render_template("register.html", form= form)
+    try:
+        form = RegisterForm(request.form)
+        print(form.validate(), form.errors) # form.validate() false olursa dönen hatayı ekrana yazdırmak için
+        if request.method == "POST" and form.validate(): #form.validate istenen tüm şartları sağlaması halinde True döner. Formda doldurulan alanlarda bir problem olup olmadığı kontrol edilir
+            name = form.name.data #formda ki name bilgisini almak
+            username = form.username.data
+            email = form.email.data
+            password = sha256_crypt.encrypt(form.password.data) #password bilgisini şifreli kaydetmek için
+
+            cursor = connection.cursor()
+            query = "INSERT INTO users (name, email, username, password) VALUES (%s, %s, %s, %s)"
+            cursor.execute(query, (name, email, username, password))
+            connection.commit() #databaseden get yapılacağı zaman bunu kullanmana gerek yok
+            cursor.close() #arka planda gereksiz kaynak kullanmamak için
+            flash("Başarıyla Kayıt Oldunuz", "success") #message, category
+            return redirect(url_for("index")) #fonksiyon ismine göre belli bir sayfaya göndermek için
+        else:
+            return render_template("register.html", form= form)
+    except Exception as e:
+        print(e)
 
 
 @app.route("/article/<string:id>")
